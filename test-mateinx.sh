7c9c311529362a4c79efeb120806b5a14f43c120
raul saavedra <raul.saavedra@gmail.com>
2022-11-29 20:45:58 +0100

    Adding error-09.json (validation of pawn positions) to tests

--- test-mateinx.sh
+++ test-mateinx.sh
@@ -9,6 +9,7 @@ python3 mateinx.py error-05.json -r -q -a
 python3 mateinx.py error-06.json -r -q
 python3 mateinx.py error-07.json -r -q
 python3 mateinx.py error-08.json -r -q
+python3 mateinx.py error-09.json -r -q
 python3 mateinx.py game-01.json -r
 
 
